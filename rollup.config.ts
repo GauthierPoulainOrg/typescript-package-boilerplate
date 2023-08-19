3c2a4e79ff803fa14e689a68388b62f45bb51889
Gauthier Poulain <poulain.gauthier16@gmail.com>
2023-08-19 21:02:02 +0200

    first

---
+++ rollup.config.ts
@@ -0,0 +1,28 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import pkg from './package.json' assert { type: 'json' }
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      name: 'gapoulai-test-package',
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' })
+    ]
+  },
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [typescript({ tsconfig: './tsconfig.json' })]
+  }
+]
---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import { hw } from '.'
+
+describe('index', () => {
+  test('test', () => {
+    expect(hw()).toBe('Hello World!')
+  })
+})
